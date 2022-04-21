b92fd2224de8c21e1953e25d9e92e010b824d141
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-22 00:18:44 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,16 +5,11 @@
 
 def minOperations(n):
     """Determines fewest number of operations needed to result in exactly n H characters in the file"""
-    ops = 0
-    H = 1
-    cpAll = 0
-
-    while H != n:
-        if n % H == 0:
-            cpAll = H
-            H = H + cpAll
-            ops = ops + 2
-        else:
-            H = H + cpAll
-            ops = ops + 1
-    return ops
+    num_ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            num_ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return num_ops
