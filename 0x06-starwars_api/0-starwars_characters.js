ca1aadbab4a3e84d6c20432ad41c59bba385c274
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-17 21:35:52 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,36 @@
 #!/usr/bin/node
 const request = require('request');
 
-const endpoint = 'https://swapi-api.hbtn.io/api';
 const filmId = process.argv[2];
 
-request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
-    if (error) return console.log(error);
-  
-    let characters = JSON.parse(body).characters;
-  
-    for (const character of characters) {
-      await new Promise((resolve, reject) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            console.log(JSON.parse(body).name);
-            resolve(body);
-          }
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmId + '/';
+
+function printMovieCharacters (url) {
+  request(url, async function (error, response, body) {
+    if (error) return console.log('An error occurred');
+
+    const characters = JSON.parse(body).characters;
+    if (!characters) return;
+
+    const promises = [];
+
+    for (const ch of characters) {
+      let promise = null;
+
+      promise = new Promise(function (resolve, reject) {
+        request(ch, function (error, response, body) {
+          if (error) return console.log('Error fetching character');
+          resolve(JSON.parse(body).name);
         });
       });
+      promises.push(promise);
+    }
+
+    const data = await Promise.all(promises);
+    for (const item of data) {
+      console.log(item);
     }
   });
+}
+
+printMovieCharacters(url);
